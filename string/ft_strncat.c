5aed8a7852c0ffaec77e2f7053ab88371355129b
LuigiFerrari <luigienzopereiraferrari@gmail.com>
2021-06-08 18:17:40 -0300

    strncatNullTerminated

--- string/ft_strncat.c
+++ string/ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: lenzo-pe <lenzo-pe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 12:44:26 by lenzo-pe          #+#    #+#             */
-/*   Updated: 2021/06/08 17:44:38 by lenzo-pe         ###   ########.fr       */
+/*   Updated: 2021/06/08 18:17:00 by lenzo-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,9 @@ char	*ft_strncat(char *dest, const char *src, size_t num)
 	if (src_len < num)
 		ft_strcpy(&dest[dest_len], src);
 	else
+	{
 		ft_strncpy(&dest[dest_len], src, num);
+		dest[dest_len + num] = '\0';
+	}
 	return (dest);
-}
-int main(void)
-{
-	
 }
--- string/ft_strncpy.c
+++ string/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: lenzo-pe <lenzo-pe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 11:23:48 by lenzo-pe          #+#    #+#             */
-/*   Updated: 2021/06/08 18:00:14 by lenzo-pe         ###   ########.fr       */
+/*   Updated: 2021/06/08 18:15:50 by lenzo-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,5 @@ char	*ft_strncpy(char *dest, const char *src, size_t num)
 	ft_memcpy(dest, src, maxlen);
 	if (maxlen < num)
 		ft_memset(dest + maxlen, '\0', num - maxlen);
-	dest[maxlen] = '\0';
 	return (dest);
 }

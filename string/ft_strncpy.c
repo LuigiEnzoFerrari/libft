9593df6f65a61e2731202f61a77be6f9e1f670d3
LuigiFerrari <luigienzopereiraferrari@gmail.com>
2021-06-25 06:25:32 -0300

    ReUploadStrncat

--- string/ft_strncpy.c
+++ string/ft_strncpy.c
@@ -6,25 +6,20 @@
 /*   By: lenzo-pe <lenzo-pe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 11:23:48 by lenzo-pe          #+#    #+#             */
-/*   Updated: 2021/06/13 10:00:02 by lenzo-pe         ###   ########.fr       */
+/*   Updated: 2021/06/25 06:23:57 by lenzo-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_string.h"
 
-char	*ft_strncat(char *dest, const char *src, size_t num)
+char	*ft_strncpy(char *dest, const char *src, size_t num)
 {
-	size_t	dest_len;
-	size_t	src_len;
+	size_t	maxlen;
 
-	dest_len = ft_strlen(dest);
-	src_len = ft_strlen(src);
-	if (src_len < num)
-		ft_strcpy(&dest[dest_len], src);
-	else
-	{
-		ft_strncpy(&dest[dest_len], src, num);
-		dest[dest_len + num] = '\0';
-	}
+	maxlen = ft_strnlen(src, num);
+	ft_memcpy(dest, src, maxlen);
+	if (maxlen < num)
+		ft_memset(dest + maxlen, '\0', num - maxlen);
+	dest[maxlen] = '\0';
 	return (dest);
 }
